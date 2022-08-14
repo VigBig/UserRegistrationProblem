791476435c499f3d429429db1ebe9680acba3fe1
VighneshBilgi <vhbilgi@gmail.com>
2022-08-14 14:24:35 +0530

    Added: Welcome to User Registration Problem

---
+++ src/com/bridgelabz/UserRegistration/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.UserRegistration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the User Registration Problem");
+
+    }
+}
