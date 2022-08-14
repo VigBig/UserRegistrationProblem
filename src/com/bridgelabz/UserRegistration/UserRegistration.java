e790ed47a3c8922c97952da5865a1bcda31e733a
VighneshBilgi <vhbilgi@gmail.com>
2022-08-14 19:34:32 +0530

    Added: UC7_Password_Rule3

--- src/com/bridgelabz/UserRegistration/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration/UserRegistration.java
@@ -66,7 +66,7 @@ public class UserRegistration {
 
     public void passwordRule(String password){
 
-        String PasswordPattern="^(?=.*[A-Z])[a-zA-Z0-9@#$%^&+]{8,}$";
+        String PasswordPattern="^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9@#$%^&+]{8,}$";
 
         Pattern pattern4=Pattern.compile(PasswordPattern);
         Matcher matcher4=pattern4.matcher(password);
@@ -75,7 +75,7 @@ public class UserRegistration {
         if(result4==true){
             System.out.println("Password is valid");
         }else{
-            System.out.println("Invaild password, minimum 8 characters required with atleast 1 of them being upper case.");
+            System.out.println("Invaild password,minimum 8 characters required with atleast 1 of them being upper case and atleast 1 of them being a number.");
         }
 
     }
